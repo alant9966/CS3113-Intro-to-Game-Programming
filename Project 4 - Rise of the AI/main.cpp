d646dea93d6f2aa5db51f06cdaf87d107fb453f7
Alan Tong <110083279+alant9966@users.noreply.github.com>
2026-04-05 03:28:36 +0000

    Added header comment block

--- Project 4 - Rise of the AI/main.cpp
+++ Project 4 - Rise of the AI/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Alan Tong
+* Assignment: Rise of the AI
+* Date due: 2025-11-08, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "CS3113/LevelC.h"
 #include "CS3113/LoseScene.h"
 
